12af2e77c0b1648af47bf5a3967e7626aaea65c0
Benjamin Tran <blt1@rice.edu>
2018-01-31 01:51:35 -0600

    Initial commit

---
+++ PhiAnalyzer/interface/utility.h
@@ -0,0 +1,67 @@
+#ifndef UTIL_H
+#define UTIL_H
+// system include files
+#include <memory>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <math.h>
+
+#include <TMath.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TNtuple.h>
+#include <TFile.h>
+#include <TROOT.h>
+#include <TSystem.h>
+#include <TString.h>
+#include <TCanvas.h>
+#include <TVector3.h>
+#include <TLorentzVector.h>
+#include <TRandom.h>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+#include "DataFormats/Candidate/interface/VertexCompositeCandidate.h"
+#include "DataFormats/Candidate/interface/VertexCompositeCandidateFwd.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/CandidateFwd.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+#include "DataFormats/CaloTowers/interface/CaloTowerCollection.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "DataFormats/TrackReco/interface/DeDxData.h"
+
+#include <Math/Functions.h>
+#include <Math/SVector.h>
+#include <Math/SMatrix.h>
+
+namespace utility
+{
+    int trackFilter(edm::Handle<reco::TrackCollection> tracks,
+            edm::Handle<reco::VertexCollection> vertices);
+}
+
+#endif
---
+++ PhiAnalyzer/plugins/utility.cc
@@ -0,0 +1,49 @@
+/*
+ * Making methods that are frequently used across modules
+ */
+
+#include "PhiAnalyzer/PhiAnalyzer/interface/utility.h"
+
+namespace utility
+{
+    int trackFilter(edm::Handle<reco::TrackCollection> tracks,
+            edm::Handle<reco::VertexCollection> vertices)
+    {
+        int Multiplicity = 0;
+        double bestvx = -999.9, bestvy = -999.9, bestvz = -999.9;
+        double bestvxError = -999.9, bestvyError = -999.9, bestvzError = -999.9;
+        const reco::Vertex & vtx = (*vertices)[0];
+        bestvx = vtx.x();
+        bestvy = vtx.y();
+        bestvz = vtx.z();
+        bestvxError = vtx.xError();
+        bestvyError = vtx.yError();
+        bestvzError = vtx.zError();
+
+        for(TrackCollection::const_iterator it = tracks->begin();
+                it != tracks->end();
+                ++it)
+        {
+            math::XYZPoint bestvtx(bestvx,bestvy,bestvz);
+
+            double dzvtx = it->dz(bestvtx);
+            double dxyvtx = it->dxy(bestvtx);
+            double dzerror = sqrt(it->dzError()*it->dzError()+bestvzError*bestvzError);
+            double dxyerror = sqrt(it->d0Error()*it->d0Error()+bestvxError*bestvyError);
+
+            if(!it->quality(reco::TrackBase::highPurity)) continue;
+            if(fabs(it->ptError())/it->pt()>0.10) continue;
+            if(fabs(dzvtx/dzerror) > 3) continue;
+            if(fabs(dxyvtx/dxyerror) > 3) continue;
+
+            double eta = it->eta();
+            double pt  = it->pt();
+
+            if(fabs(eta)>2.4) continue;
+            if(pt<=0.4) continue;
+            Multiplicty++;
+        }
+
+        return Multiplicty;
+    }
+}
