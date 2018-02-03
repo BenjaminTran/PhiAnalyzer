9d6fc180f180222614890414fb8f70c57708c0ce
Benjamin Tran <blt1@rice.edu>
2018-02-03 03:33:05 -0600

    Testing

--- PhiAnalyzer/plugins/PhiSelector.cc
+++ PhiAnalyzer/plugins/PhiSelector.cc
@@ -31,7 +31,7 @@ PhiSelector::PhiSelector(const edm::ParameterSet& iConfig)
     _trkSrc = consumes<reco::TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("trkSrc"));
     _vtxSrc = consumes<reco::VertexCollection>(iConfig.getUntrackedParameter<edm::InputTag>("vtxSrc"));
     _Dedx_Harmonic2 = consumes<edm::ValueMap<reco::DeDxData> >(edm::InputTag("dedxHarmonic2"));
-    _Dedx_Trunc40 = consumes<edm::ValueMap<reco::DeDxData> >(edm::InputTag("dedxTruncated40"));
+    //_Dedx_Trunc40 = consumes<edm::ValueMap<reco::DeDxData> >(edm::InputTag("dedxTruncated40"));
 }
 
 
@@ -101,8 +101,8 @@ PhiSelector::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    //Vectors to hold kaons to perform combinatorial mass reconstruction. Following PEN naming scheme
    std::vector<PhiSelector::kaon> PKp_Harm; //Positive charged
    std::vector<PhiSelector::kaon> PKm_Harm; //Negative charged
-   std::vector<PhiSelector::kaon> PKp_Trun; //Positive charged
-   std::vector<PhiSelector::kaon> PKm_Trun; //Negative charged
+   //std::vector<PhiSelector::kaon> PKp_Trun; //Positive charged
+   //std::vector<PhiSelector::kaon> PKm_Trun; //Negative charged
 
    h_nEvt->Fill(1);
 
@@ -117,8 +117,8 @@ PhiSelector::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    if(DeDx_Harm.isValid()) cout << "GOODZ" << endl;
    else cout << "BADZ" << endl;
 
-   edm::Handle<edm::ValueMap<reco::DeDxData> > DeDx_Trun;
-   iEvent.getByToken(_Dedx_Trunc40,DeDx_Trun);
+   //edm::Handle<edm::ValueMap<reco::DeDxData> > DeDx_Trun;
+   //iEvent.getByToken(_Dedx_Trunc40,DeDx_Trun);
 
    // Multiplicity selection
    int mult = utility::trackFilter(tracks,vertices);
@@ -136,16 +136,16 @@ PhiSelector::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
        //Fill in the dEdx histograms
        DeDxFiller(track_bundle,DeDx_Harm,h_Dedx_p_Harm);
-       DeDxFiller(track_bundle,DeDx_Trun,h_Dedx_p_Trun);
+       //DeDxFiller(track_bundle,DeDx_Trun,h_Dedx_p_Trun);
 
        // Make the vector of kaons to calculate invariant mass at the end
        FillKaonContainer(track_bundle,DeDx_Harm,PKp_Harm,PKm_Harm);
-       FillKaonContainer(track_bundle,DeDx_Trun,PKp_Trun,PKm_Trun);
+       //FillKaonContainer(track_bundle,DeDx_Trun,PKp_Trun,PKm_Trun);
 
        h_charge->Fill(track_bundle.track->charge());
    }
    CombinatorialMass(PKp_Harm,PKm_Harm,h_mass_Harm);
-   CombinatorialMass(PKp_Trun,PKm_Trun,h_mass_Trun);
+   //CombinatorialMass(PKp_Trun,PKm_Trun,h_mass_Trun);
 }
 
 
